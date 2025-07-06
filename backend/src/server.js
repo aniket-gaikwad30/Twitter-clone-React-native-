3d048943c811d4c8dd76bde11595551d96c8b573
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-07-06 15:36:13 +0530

    Backend done

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,44 @@
-import expres from "express";
+import express from "express";
+import cors from "cors";
+import { clerkMiddleware } from "@clerk/express";
+import { ENV } from "./config/env.js";
+import { connect } from "mongoose";
+import { connectDB } from "./config/db.js";
+import userRoutes from "./routes/user.route.js";
+import postRoutes from "./routes/post.route.js";
+import commentRoutes from "./routes/comment.route.js";
 
+const app = express();
 
-const app = expres();
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+app.use(clerkMiddleware());
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+
+
+app.use((err,req,res, next)=>{
+  console.log("Unhandled error ", err)
+  res.status(500).json({error : err.message || "Internal server error"})
+})
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () => {
+      console.log("Server is running on port 3000");
+    });
+  } catch (error) {
+    console.error("Failed to start server ", error.message);
+  }
+};
+
+startServer();
