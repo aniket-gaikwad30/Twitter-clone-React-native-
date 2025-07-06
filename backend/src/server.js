5c7833ed2d34fd1e9c647e6258fc23e9f4926863
Aniket Gaikwad <aniketgaikwadhere30@gmail.com>
2025-07-06 18:02:57 +0530

    arcjet added

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,6 +7,9 @@ import { connectDB } from "./config/db.js";
 import userRoutes from "./routes/user.route.js";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import notificationRoutes from "./routes/notification.route.js";
+import { arcjetMiddleware } from "./middleware/arcjet.middleware.js";
+
 
 const app = express();
 
@@ -14,6 +17,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(clerkMiddleware());
+app.use(arcjetMiddleware)
 
 app.get("/", (req, res) => {
   res.send("Hello, World!");
@@ -22,6 +26,7 @@ app.get("/", (req, res) => {
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 
 app.use((err,req,res, next)=>{
